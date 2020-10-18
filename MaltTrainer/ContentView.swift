1d73f598821cbb7fac5d2073ff3507b8a4040b14
BattyCathy <austinroach@gmail.com>
2020-10-17 17:23:05 -0700

    Initial Commit

---
+++ MaltTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaltTrainer
+//
+//  Created by Austin Roach on 10/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MaltTrainer/MaltTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaltTrainerApp.swift
+//  MaltTrainer
+//
+//  Created by Austin Roach on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct MaltTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
